d208840e14315852a9af348ac0e4e62900b1b670
PrivOci <74867724+PrivOci@users.noreply.github.com>
2021-05-10 17:41:55 +0100

    Update facebook.py

--- backend/extract_details/facebook.py
+++ backend/extract_details/facebook.py
@@ -25,8 +25,8 @@ async def get_facebook_page_source(details: dict):
         video_entry = {
             "platform": FACEBOOK,
             "id": details["id"],
-            "title": post["text"] if post["text"]
-            else post["time"].strftime("%m/%d/%Y"),
+            "title": post["text"][:100] if post["text"]
+            else post["time"].strftime("%d/%m/%Y"),
             "author": post["username"],
             "channelUrl": post["user_url"].split("?")[0],
             "thumbnailUrl": post["video_thumbnail"],
