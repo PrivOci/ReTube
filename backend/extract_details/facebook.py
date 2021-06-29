16d235b8c3562ac9d50e61612360270992b005ec
PrivOci <74867724+PrivOci@users.noreply.github.com>
2021-06-29 10:42:49 +0100

    fix if fb fails

--- backend/extract_details/facebook.py
+++ backend/extract_details/facebook.py
@@ -14,37 +14,43 @@ class FacebookProcessor:
         TODO: maybe optional FB account
         """
         page_name = details["id"]
-        try:
-            posts_iterator = get_posts(page_name, page_limit=3)
-        except:
-            return None
         data_dict = {}
         data_dict["ready"] = False
         data_dict["platform"] = self.FACEBOOK
+        try:
+            posts_iterator = get_posts(page_name, page_limit=3)
+        except:
+            return data_dict
+
         video_entries = []
-        for post in posts_iterator:
-            if not post["video"] or post["is_live"]:
-                continue
-            video_entry = {
-                "platform": self.FACEBOOK,
-                "id": details["id"],
-                "title": post["text"][:100] if post["text"]
-                else post["time"].strftime("%d/%m/%Y"),
-                "author": post["username"],
-                "channelUrl": post["user_url"].split("?")[0],
-                "thumbnailUrl": post["video_thumbnail"],
-                "createdAt": int(post["time"].timestamp()) * 1000,
-                "videoUrl": post["post_url"],
-                "duration": post["video_duration_seconds"],
-                "views": post["video_watches"],
-            }
+        try:
+            for post in posts_iterator:
+                if not post["video"] or post["is_live"]:
+                    continue
+                video_entry = {
+                    "platform": self.FACEBOOK,
+                    "id": details["id"],
+                    "title": post["text"][:100] if post["text"]
+                    else post["time"].strftime("%d/%m/%Y"),
+                    "author": post["username"],
+                    "channelUrl": post["user_url"].split("?")[0],
+                    "thumbnailUrl": post["video_thumbnail"],
+                    "createdAt": int(post["time"].timestamp()) * 1000,
+                    "videoUrl": post["post_url"],
+                    "duration": post["video_duration_seconds"],
+                    "views": post["video_watches"],
+                }
 
-            video_url = video_entry["videoUrl"].split("facebook.com/")[1]
-            self.global_video_details_url[video_url] = video_entry
-            self.global_video_details_url[video_url]["description"] = post["post_text"]
-            self.global_video_details_url[video_url]["streamUrl"] = post["video"]
+                video_url = video_entry["videoUrl"].split("facebook.com/")[1]
+                self.global_video_details_url[video_url] = video_entry
+                self.global_video_details_url[video_url]["description"] = post["post_text"]
+                self.global_video_details_url[video_url]["streamUrl"] = post["video"]
 
-            video_entries.append(video_entry)
+                video_entries.append(video_entry)
+        except:
+            return data_dict
+        if not video_entries:
+            return data_dict
         data_dict["ready"] = True
         data_dict["content"] = video_entries if len(video_entries) else None
         return data_dict
